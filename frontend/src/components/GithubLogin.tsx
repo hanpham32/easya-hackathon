6f9449bbcdd42c8fdace174763417bcf2a778447
Han <phamhan@uw.edu>
2024-05-30 01:10:22 -0500

    feat: get started page
    - Add Sui wallet connect interface
    - Add Github login

--- frontend/src/components/GithubLogin.tsx
+++ frontend/src/components/GithubLogin.tsx
@@ -1,6 +1,20 @@
-import { GithubLoginButton } from 'react-social-login-buttons'
-import { LoginSocialGithub } from 'reactjs-social-login'
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+import { GithubLoginButton } from "react-social-login-buttons"
 
 export default function GithubLogin() {
-
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <div>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <GithubLoginButton onClick={() => signIn()} />
+    </div>
+  )
 }
