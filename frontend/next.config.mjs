ea79c72fbda7e59d61e13adf5a33fabb4c4d4a6d
Han <phamhan@uw.edu>
2024-05-30 16:46:11 -0500

    chores: add img domain for nextjs config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['cdn.starknet.quest', 'randomuser.me'],
+  },
+};
 
 export default nextConfig;
