31268b1f8dafb49dd0423778d5a4b91dc0f17c25
Han <phamhan@uw.edu>
2024-05-31 03:05:57 -0500

    fix: reset next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-import withMDX from "@next/mdx"
-
-const mdxConfig = withMDX({
-  extension: /\.mdx?$/
-});
-
 const nextConfig = {
   images: {
     domains: ['cdn.starknet.quest', 'randomuser.me'],
   },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
 };
 
-export default { mdxConfig, nextConfig };
+export default nextConfig;
