3a0e4c70e7075b18686706f281e9795efa2af8f4
Han Pham <phamhan@uw.edu>
2024-06-13 17:24:42 -0700

    revamp nav

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,38 +1,51 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useWallet } from "@suiet/wallet-kit";
 import SignOutBtn from "./SignOutBtn";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Nav() {
   const wallet = useWallet();
   const { data: session } = useSession();
 
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-100 shadow-md">
+    <div className="flex justify-between items-center px-[24em] p-4">
       <div className="flex items-center">
-        <Image src="/frog.png" alt="Logo" className="mr-2" width={64} height={64} />
+        <Image
+          src="/frog.png"
+          alt="Logo"
+          className="mr-2"
+          width={64}
+          height={64}
+        />
         <span className="text-xl font-bold">Web3 Workshop</span>
-      </div>
-      <div className="flex items-center">
         <Link href="/">
-          <span className="mr-4 px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-blue-600 transition-colors duration-200">Home</span>
+          <span className="ml-12 px-4 py-2 hover:bg-gray-200 hover:rounded-full transition-all duration-200">
+            Home
+          </span>
         </Link>
         <Link href="/workshops">
-          <span className="mr-4 px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-blue-600 transition-colors duration-200">Workshops</span>
+          <span className="mr-4 px-4 py-2 hover:bg-gray-200 hover:rounded-full transition-all llduration-200">
+            Workshops
+          </span>
         </Link>
-        {
-          (wallet.connected && session) ? (
-            <div className="flex items-center">
-              <Link href="/dashboard">
-                <span className="mr-4 px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-blue-600 transition-colors duration-200">Dashboard</span>
-              </Link>
-              <SignOutBtn />
-            </div>
-          ) : null
-        }
+      </div>
+      <div className="flex items-center">
+        {wallet.connected && session ? (
+          <div className="flex items-center">
+            <Link href="/dashboard">
+              <span className="mr-4 px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-blue-600 transition-colors duration-200">
+                Dashboard
+              </span>
+            </Link>
+            <SignOutBtn />
+          </div>
+        ) : (
+          <Button variant="outline">Log In</Button>
+        )}
       </div>
     </div>
-  )
+  );
 }
--- frontend/src/layout/Hero.tsx
+++ frontend/src/layout/Hero.tsx
@@ -1,35 +1,54 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section>
       <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 overflow-hidden md:px-8 md:flex">
-        <div className='flex-none space-y-5 max-w-xl'>
+        <div className="flex-none space-y-5 max-w-xl">
           <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl">
             Workshops Hub
           </h1>
           <p>
-            On Workshops Hub, you don&apos;t just learn -- you build, deploy real projects, and mastering your craft with every push
+            On Workshops Hub, you don&apos;t just learn -- you build, deploy
+            real projects, and mastering your craft with every push
           </p>
-          <div className='flex items-center gap-x-3 sm:text-sm'>
-            <a href="/get-started" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+          <div className="flex items-center gap-x-3 sm:text-sm">
+            <a
+              href="/get-started"
+              className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
+            >
               Get started
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                  clipRule="evenodd"
+                />
               </svg>
             </a>
           </div>
         </div>
-        <div className='flex-1 hidden md:block'>
+        <div className="flex-1 hidden md:block">
           {/* Replace with your image */}
-          <Image src="./hero-bg.svg" className="max-w-xl" color="red" alt="hero-bg" width={500} height={500} />
+          <Image
+            src="./hero-bg.svg"
+            className="max-w-xl"
+            color="red"
+            alt="hero-bg"
+            width={500}
+            height={500}
+          />
         </div>
       </div>
     </section>
-  )
+  );
 }
-
 
 // return (
 //   <div className="h-screen flex justify-center items-center bg-slate-200">
