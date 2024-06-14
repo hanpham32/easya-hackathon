9e03dc55b7c696b1002d3a1294450e5ba89ba115
Han Pham <phamhan@uw.edu>
2024-06-13 17:32:46 -0700

    misc: remove react-social-btns library

--- frontend/src/components/GithubLogin.tsx
+++ frontend/src/components/GithubLogin.tsx
@@ -1,20 +1,19 @@
-"use client"
-import { useSession, signIn, signOut } from "next-auth/react"
-import { GithubLoginButton } from "react-social-login-buttons"
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function GithubLogin() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
   if (session && session.user) {
     return (
       <div>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </div>
-    )
+    );
   }
   return (
     <div>
-      <GithubLoginButton onClick={() => signIn()} />
+      <button onClick={() => signIn()}>Authenticate your Github</button>
     </div>
-  )
+  );
 }
